8fdd67a3722a1fbe7703a7aa6c772aea29edbe35
MyoThetKoKo <myothiha2070@gmail.com>
2016-11-21 13:35:59 +0630

    Update index.js

--- index.js
+++ index.js
@@ -39,23 +39,23 @@ app.post('/webhook/', function (req, res) {
         continue
       }
       else if (text === 'Topstories'){
-        sendTopstoriesMessage(sender)
+        sendlistMessage(sender)
         continue
       }
       else if (text === 'News'){
-        sendNewsMessage(sender)
+        sendlistMessage(sender)
         continue
       }
       else if (text === 'Entertainment'){
-        sendEntertainmentMessage(sender)
+        sendlistMessage(sender)
         continue
       }
       else if (text === 'lifestyle'){
-        sendlifestyleMessage(sender)
+        sendlistMessage(sender)
         continue
       }
       else if (text === 'Sports'){
-        sendSportsMessage(sender)
+        sendlistMessage(sender)
         continue
       }
       sendGreetingMessage(sender)
@@ -104,7 +104,7 @@ function sendGenericMessage(sender)
         "elements": [{
           "title": "Duwun1",
           "subtitle": "Element #1 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14670626_657726541056112_2265577433017880324_n.jpg?oh=131e530db7f8207922de974e42f6a047&oe=58C6191A",
           "buttons": [{
             "type": "web_url",
             "url": "http://www.duwun.com.mm/sports/epl/-id5741234.html",
@@ -117,7 +117,7 @@ function sendGenericMessage(sender)
         }, {
           "title": "Duwun2",
           "subtitle": "Element #2 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14666250_653700034792096_6286400383312462415_n.jpg?oh=b8568feb46010026073a41876d7ff5f2&oe=58BC9202",
           "buttons": [{
             "type": "postback",
             "title": "Postback",
@@ -154,7 +154,7 @@ function sendTopstoriesMessage(sender)
         "elements": [{
           "title": "Duwun1",
           "subtitle": "Element #1 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14910494_662480397247393_5785870071355333292_n.jpg?oh=7b630439c4f3a6538b2bf3654dbfca6d&oe=5889E12C",
           "buttons": [{
             "type": "web_url",
             "url": "http://www.duwun.com.mm/sports/epl/-id5741234.html",
@@ -167,7 +167,7 @@ function sendTopstoriesMessage(sender)
         }, {
           "title": "Duwun2",
           "subtitle": "Element #2 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14695309_659017747593658_7823234448286722387_n.jpg?oh=01b1df2658a85e53a1fc91f085b59011&oe=58BCEF8A",
           "buttons": [{
             "type": "postback",
             "title": "Postback",
@@ -204,7 +204,7 @@ function sendNewsMessage(sender)
         "elements": [{
           "title": "Duwun1",
           "subtitle": "Element #1 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14947665_665057290323037_5916774489129921508_n.jpg?oh=bfd476b32d2022b6bcc9644e0f55b8fb&oe=588A9A8B",
           "buttons": [{
             "type": "web_url",
             "url": "http://www.duwun.com.mm/sports/epl/-id5741234.html",
@@ -217,7 +217,7 @@ function sendNewsMessage(sender)
         }, {
           "title": "Duwun2",
           "subtitle": "Element #2 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14962639_665057306989702_4394495154565763093_n.jpg?oh=f7e6f1a25ee1bcdd5dd69264a7bd197e&oe=58B84197",
           "buttons": [{
             "type": "postback",
             "title": "Postback",
@@ -254,7 +254,7 @@ function sendEntertainmentMessage(sender)
         "elements": [{
           "title": "Duwun1",
           "subtitle": "Element #1 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/15036186_667298540098912_6919527525160284544_n.jpg?oh=d34b7ca182fab60e8139a51fb8d8f739&oe=58C97071",
           "buttons": [{
             "type": "web_url",
             "url": "http://www.duwun.com.mm/sports/epl/-id5741234.html",
@@ -267,7 +267,7 @@ function sendEntertainmentMessage(sender)
         }, {
           "title": "Duwun2",
           "subtitle": "Element #2 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14953575_667298550098911_9186132226253692818_n.jpg?oh=ee9d421d662f1c85c2e939f26ab2ae37&oe=58D577FB",
           "buttons": [{
             "type": "postback",
             "title": "Postback",
@@ -304,7 +304,7 @@ function sendSportsMessage(sender)
         "elements": [{
           "title": "Duwun1",
           "subtitle": "Element #1 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/15037139_671483689680397_7714670410490897561_n.jpg?oh=00fd19a760baddea652906266cbcacd2&oe=5889914D",
           "buttons": [{
             "type": "web_url",
             "url": "http://www.duwun.com.mm/sports/epl/-id5741234.html",
@@ -317,7 +317,7 @@ function sendSportsMessage(sender)
         }, {
           "title": "Duwun2",
           "subtitle": "Element #2 of an hscroll",
-          "image_url": "https://scontent.fbkk4-2.fna.fbcdn.net/v/t1.0-9/12342591_814669595343202_7303905748891865152_n.png?oh=d2eaed4788b2c1e429e4f1fd474be99c&oe=588D1A27",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/14925771_666715536823879_1924831144549562241_n.jpg?oh=447b1bffe9825b7ff802db821c02c41c&oe=58CECA1F",
           "buttons": [{
             "type": "postback",
             "title": "Postback",
@@ -354,7 +354,7 @@ function sendlifestyleMessage(sender)
         "elements": [{
           "title": "Duwun1",
           "subtitle": "Element #1 of an hscroll",
-          "image_url": "https://drive.google.com/open?id=0BxuIjI-2X6CPTUpzUHdwR2wwYnBKQkZKb3BPUXNGd0dNOXo4",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-0/c0.11.200.200/p200x200/15036693_672096482952451_286439968195064488_n.jpg?oh=d69c842f166981c66476f760a0a27e54&oe=58BCB9A9",
           "buttons": [{
             "type": "web_url",
             "url": "https://drive.google.com/open?id=0BxuIjI-2X6CPZHY3b19Lc3VSUVlqMWxVNUtCV1FWSUpwcmFz",
@@ -367,7 +367,7 @@ function sendlifestyleMessage(sender)
         }, {
           "title": "Duwun2",
           "subtitle": "Element #2 of an hscroll",
-          "image_url": "https://drive.google.com/open?id=0BxuIjI-2X6CPb2JXRnBTbFVacTE4aGszVUJRSGJISkRmQ1RF",
+          "image_url": "https://scontent.fbkk3-1.fna.fbcdn.net/v/t1.0-9/15056258_672096459619120_3328037144239154473_n.jpg?oh=e4a56ed7b188732c8eb65d275288c5ed&oe=58B8BA50",
           "buttons": [{
             "type": "postback",
             "title": "Postback",
